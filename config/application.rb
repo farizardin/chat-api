034b87d73675f4e7ed856d1359d0ba2c97d6261a
farizardin <fariz.ardin@gmail.com>
2020-12-11 14:46:29 +0700

    add jwt lib

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module ChatApi
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join("app/response")
+    config.eager_load_paths << Rails.root.join("app/response"), Rails.root.join("lib")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
