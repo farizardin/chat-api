19a71ca9c5551805989ae47896518ffdebce58c1
farizardin <fariz.ardin@gmail.com>
2020-12-11 14:47:33 +0700

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/chat", to: "chat#index"
+  get "/chat/open", to: "chat#index"
   post "/chat/send", to: "chat#send_message"
   post "/register", to: "user#create"
+  post "/login", to: "auth#login"
 end
