1247e37f323d272a2471fd9f20669ef1fec0f696
farizardin <fariz.ardin@gmail.com>
2020-12-11 12:57:12 +0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/chat", to: "chat#index"
+  post "/chat/send", to: "chat#send_message"
   post "/register", to: "user#create"
 end
