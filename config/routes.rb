f16a1c430cc5094f8209ef6bf6e3da86e783effa
farizardin <fariz.ardin@gmail.com>
2020-12-11 16:20:32 +0700

    modify open conversation route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/chat", to: "chat#index"
-  get "/chat/open", to: "chat#index"
+  get "/chat/open", to: "chat#open_conversation"
   post "/chat/send", to: "chat#send_message"
   post "/register", to: "user#create"
   post "/login", to: "auth#login"
