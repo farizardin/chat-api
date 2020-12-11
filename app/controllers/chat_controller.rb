02348b7120017816ca623f5643afabefbe8c0133
farizardin <fariz.ardin@gmail.com>
2020-12-11 16:18:45 +0700

    add self send message handler

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -9,8 +9,8 @@ class ChatController < ApplicationController
             @sender = chat_params[:sender]
             @destination = chat_params[:destination]
             @get_conversations_data = Conversation.select("conversations.message,users.name, conversations.id, conversations.created_at")
-                                    .where('conversations.sender =? and conversations.destination =?', @sender,@destination)
-                                    .or(Conversation.where('conversations.sender =? and conversations.destination =?', @destination,@sender))
+                                    .where('conversations.sender =? and conversations.destination =?', @sender, @destination)
+                                    .or(Conversation.where('conversations.sender =? and conversations.destination =?', @destination, @sender))
                                     .joins("INNER JOIN users ON users.id = conversations.sender").order(created_at: :desc)
 
             @conversations_data = {:conversations => @get_conversations_data}
@@ -35,32 +35,33 @@ class ChatController < ApplicationController
             @sender = @current_user.id
             @destination = chat_params[:destination]
             @message = chat_params[:message]
-            
-            @conversations = Conversation.new
-            @conversations.sender = @sender
-            @conversations.destination = @destination
-            @conversations.message = @message
-            @conversations.unread = 1
-            if @conversations.save
-                @get_conversations_data = Conversation.select("conversations.message,users.name, conversations.id, conversations.created_at")
-                                        .where('conversations.sender =? and conversations.destination =?', @sender,@destination)
-                                        .or(Conversation.where('conversations.sender =? and conversations.destination =?', @destination,@sender))
-                                        .joins("INNER JOIN users ON users.id = conversations.sender").order(created_at: :desc)
-
-                @conversations_data = {:conversations => @get_conversations_data}
-                response_data = ResponseObject.new
-                response_data.set_success_code
-                response_data.sent_message
-                response_data.set_data(@conversations_data)
-                render json: response_data
+            if @destination.to_i != @sender
+                @message_data = Conversation.new
+                @message_data.sender = @sender
+                @message_data.destination = @destination
+                @message_data.message = @message
+                @message_data.unread = 1
+                @read_message = Conversation.where(:sender => @sender).where(:destination => @destination).update_all(:unread => 0)
+                if @message_data.save
+                    @data = {:message_data => @message_data}
+                    response_data = ResponseObject.new
+                    response_data.set_success_code
+                    response_data.sent_message
+                else
+                    @data = {:error => @message_data.errors}
+                    response_data = ResponseObject.new
+                    response_data.set_success_code
+                    response_data.unsent_message
+                end
             else
-                @data = {:error => @conversations.errors}
+                @data = {:error => "Tidak diperbolehkan mengirimkan ke destination yang sama dengan sender"}
                 response_data = ResponseObject.new
                 response_data.set_success_code
                 response_data.unsent_message
-                response_data.set_data(@data)
-                render json: response_data
             end
+            response_data.set_data(@data)
+            render json: response_data
+            
         rescue => error
             @data = {:error => error.message}
             response_data = ResponseObject.new
@@ -74,11 +75,11 @@ class ChatController < ApplicationController
     def open_conversation
         begin
             @sender = chat_params[:sender]
-            @destination = chat_params[:destination]
-
+            @destination = @current_user.id
+            @read_message = Conversation.where(:sender => @sender).where(:destination => @destination).update_all(:unread => 0)
             @get_conversations_data = Conversation.select("conversations.message,users.name, conversations.id, conversations.created_at")
-                                    .where('conversations.sender =? and conversations.destination =?', @sender,@destination)
-                                    .or(Conversation.where('conversations.sender =? and conversations.destination =?', @destination,@sender))
+                                    .where('conversations.sender =? and conversations.destination =?', @sender, @destination)
+                                    .or(Conversation.where('conversations.sender =? and conversations.destination =?', @destination, @sender))
                                     .joins("INNER JOIN users ON users.id = conversations.sender").order(created_at: :desc)
             
             @conversations_data = {:conversations => @get_conversations_data}
