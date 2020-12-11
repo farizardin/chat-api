0e79e1c558c2857fa0187cd09dc9c58ca35af3ab
farizardin <fariz.ardin@gmail.com>
2020-12-11 16:17:55 +0700

    modify handler

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,20 +1,27 @@
 require 'response_object'
+require 'json_web_token'
 class UserController < ApplicationController
     def create
-        @users = User.new(user_params)
-        if @users.save
-            @data = @users
-            response_data = ResponseObject.new
+        begin
+            @users = User.new(user_params)
+            if @users.save
+                @data = {:user => @users} 
+                response_data = ResponseObject.new
+                response_data.set_custom_message("User berhasil disimpan")
+            else
+                errors = {:errors => @users.errors.full_messages}
+                @data = errors
+                response_data = ResponseObject.new
+                response_data.set_custom_message("User gagal disimpan")
+            end
             response_data.set_success_code
-            response_data.set_custom_message("User berhasil disimpan")
             response_data.set_data(@data)
             render json: response_data
-        else
-            errors = {:errors => @users.errors.full_messages}
-            @data = errors
+        rescue => error
+            @data = {:error => error.message}
             response_data = ResponseObject.new
-            response_data.set_success_code
-            response_data.set_custom_message("User gagal disimpan")
+            response_data.set_error_server_code
+            response_data.set_error_server_message
             response_data.set_data(@data)
             render json: response_data
         end
