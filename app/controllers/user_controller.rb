eb97e082046410e2aec3923dbedc8a821adb8fa2
farizardin <fariz.ardin@gmail.com>
2020-12-12 16:41:52 +0700

    add show users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 require 'response_object'
 require 'json_web_token'
 class UserController < ApplicationController
+    before_action :authorization, except: :create
     def create
         begin
             @users = User.new(user_params)
@@ -16,6 +17,25 @@ class UserController < ApplicationController
             end
             response_data.set_success_code
             response_data.set_data(@data)
+            render json: response_data
+        rescue => error
+            @data = {:error => error.message}
+            response_data = ResponseObject.new
+            response_data.set_error_server_code
+            response_data.set_error_server_message
+            response_data.set_data(@data)
+            render json: response_data
+        end
+    end
+
+    def show
+        begin
+            @user_id = @current_user.id
+            @users = User.select(:id,:name, :username, :email).where("users.id != ?",@user_id)
+            response_data = ResponseObject.new
+            response_data.set_success_message
+            response_data.set_success_code
+            response_data.set_data(@users)
             render json: response_data
         rescue => error
             @data = {:error => error.message}
